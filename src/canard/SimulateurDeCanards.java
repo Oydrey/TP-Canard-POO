85ef8b98a58ded01cc7697af00e7cb6a38ace8a9
Oydrey <oydrey@gmail.com>
2020-11-04 10:11:25 +0100

    partie 3 ok

--- src/canard/SimulateurDeCanards.java
+++ src/canard/SimulateurDeCanards.java
@@ -8,17 +8,18 @@ public class SimulateurDeCanards {
 	}
 	
 	void simuler() {
-		Cancaneur colvert = new Colvert();
-		Cancaneur mandarin = new Mandarin();
-		Cancaneur appelant = new Appelant();
-		Cancaneur canardEnPlastique = new CanardEnPlastique();
-		AdaptateurDOie canardOie = new AdaptateurDOie(new Oie());
+		Cancaneur colvert = new CompteurDeCouacs(new Colvert());
+		Cancaneur mandarin = new CompteurDeCouacs(new Mandarin());
+		Cancaneur appelant = new CompteurDeCouacs(new Appelant());
+		Cancaneur canardEnPlastique = new CompteurDeCouacs(new CanardEnPlastique());
+		Cancaneur canardOie = new CompteurDeCouacs(new AdaptateurDOie(new Oie()));
 		System.out.println("Simulateur de canards\n");
 		simuler(colvert);
 		simuler(mandarin);
 		simuler(appelant);
 		simuler(canardEnPlastique);
 		simuler(canardOie);
+		System.out.println("\nNous avons compté " + CompteurDeCouacs.getCouacs() + " couacs.");
 	}
 	
 	void simuler(Cancaneur canard) {
